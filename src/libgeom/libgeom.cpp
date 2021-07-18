2ae198c7e93ba4f81ddc0cc9cd3e61f94e8be3c2
shuhei-matsushima <shuhei-matsushima@outlook.jp>
2021-07-18 19:32:30 +0900

    全てのテストに通る様にしました。

--- src/libgeom/libgeom.cpp
+++ src/libgeom/libgeom.cpp
@@ -11,6 +11,7 @@ void copy_vector4_array(float* dst, const float* src, int num)
 {
 #if 1
 	// ToDo: SIMD計算を使って実装して下さい
+
 	for (int i = 0; i < num * UNIT; i += UNIT)
 	{
 		//__m128 … SSE/AVXの変数型、128 bitのレジスタを自由に切って使う（float型(32bit)4個分）
@@ -42,6 +43,7 @@ void add_vector4_array(float* dst, const float* src0, const float* src1, int num
 {
 #if 1
 	// ToDo: SIMD計算を使って実装して下さい
+
 	for (int i = 0; i < num * UNIT; i += UNIT)
 	{
 		//src0,src1 のデータをレジスタ ps0,ps1 に 128bit（32bit * 4個）ロードする
@@ -78,6 +80,51 @@ void apply_matrix_vector4_array(float* dst, const float* src, const float* matri
 {
 #if 1
 	// ToDo: SIMD計算を使って実装して下さい
+
+	//matrix[4][4] を1行ずつレジスタ に 128bit（32bit * 4個）ロードする
+	__m128 ma_row[UNIT];
+	for (int i = 0; i < UNIT; i++)
+	{
+		ma_row[i] = _mm_load_ps(matrix + UNIT * i);
+	}
+
+	for (int i = 0; i < num * UNIT; i += UNIT)
+	{
+		//src のデータをレジスタ pd に 128bit（32bit * 4個）ロードする
+		__m128 ps = _mm_load_ps(src + i);
+
+		//効率の良いやり方が分からず、回りくどくなってしまいました。すみません。
+
+		__m128 result[UNIT];            //1行毎の乗算した要素を格納（加算はまだ）
+		float tempArray[UNIT * UNIT];   //乗算した要素を一旦纏める
+		for (int j = 0; j < UNIT; j++)
+		{
+			result[j] = _mm_mul_ps(ma_row[j], ps);
+			_mm_store_ps(tempArray + UNIT * j, result[j]);
+		}
+
+		__m128 column[UNIT];   //tempArray[16]を列ごとに纏める
+		for (int j = 0; j < UNIT; j++)
+		{
+			float temp[UNIT];
+			for (int k = 0; k < UNIT; k++)
+			{
+				temp[k] = tempArray[j + UNIT * k];
+			}
+			column[j]= _mm_load_ps(temp);
+		}
+
+		//最後に纏めて加算を行う
+		__m128 pd = column[0];
+		for (int j = 1; j < UNIT; j++)
+		{
+			pd = _mm_add_ps(pd, column[j]);
+		}
+
+		//レジスタ pd のデータをメモリのdstに128bit（32bit * 4個）ストアする
+		_mm_store_ps(dst + i, pd);
+	}
+
 #else
 	float* pd = dst;
 	const float* ps = src;
